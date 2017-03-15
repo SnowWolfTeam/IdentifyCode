276ba3a9b29f5fa4d3a226d85a2fdfc3b8483000
THR <anlun214@qq.com>
2017-03-15 18:23:44 +0800

    change a litte

--- Exception/IdentifyCodeException.php
+++ Exception/IdentifyCodeException.php
@@ -2,10 +2,10 @@
 namespace IdentifyCode\Exception;
 class IdentifyCodeException extends \Exception
 {
-    const PARAMS_NULL = 0x1000;
-    const LAST_STAMPE_TYPE_ERROR = 0x1001;
-    const LAST_STAPME_BIGGER_EQUEAL_0 = 0x1002;
-    const TIME_INTERVAL_INT = 0x1003;
+    const PARAMS_NULL = 1000;
+    const TIME_INTERVAL_INT = 1001;
+    const LAST_STAMPE_TYPE_ERROR = 1002;
+    const LAST_STAPME_BIGGER_EQUEAL_0 = 0x1003;
     const TIME_INTERVAL_BIGGER_EQUEAL_0 = 0x1004;
 
     public function __construct($message, $code)
